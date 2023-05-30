1156db6ccce356df4ed3e256e536bce9210f1bef
James McDonald <james@toggen.com.au>
2023-05-31 07:54:35 +1000

    Add README and dotenv example

---
+++ README.md
@@ -0,0 +1,7 @@
+# Read Counter in Adam 6251 Remote I/O Module with Modbus TCP
+
+
+[https://github.com/aldas/modbus-tcp-client](https://github.com/aldas/modbus-tcp-client)
+
+
+ADAM-6200_User_Manual_Ed.5_FINAL.pdf
--- get.php
+++ get.php
@@ -6,13 +6,19 @@ use ModbusTcpClient\Network\NonBlockingClient;
 
 require './vendor/autoload.php';
 
-$address = 'tcp://10.73.19.45:502';
-$unitID = 73; // also known as 'slave ID'
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
+$address = $_ENV['TARGET'];
+
+$unitID = $_ENV['SLAVE_ID']; // also known as 'slave ID'
+
 $fc3 = ReadRegistersBuilder::newReadHoldingRegisters($address, $unitID)
     // ->bit(256, 15, 'pump2_feedbackalarm_do')
     // will be split into 2 requests as 1 request can return only range of 124 registers max
-    ->uint16(0, 'counter_1')
-    ->uint16(1, 'counter_2')
+    ->uint16(0, 'counter_1') // 40001
+    ->uint16(1, 'counter_2') // 40002
     // will be another request as uri is different for subsequent int16 register
     // ->useUri('tcp://127.0.0.1:5023')
     // ->string(
@@ -32,6 +38,7 @@ $fc3 = ReadRegistersBuilder::newReadHoldingRegisters($address, $unitID)
 // this will use PHP non-blocking stream io to recieve responses
 $responseContainer = (new NonBlockingClient(['readTimeoutSec' => 0.2]))->sendRequests($fc3);
 $data = $responseContainer->getData();
+
 print_r($data); // array of assoc. arrays (keyed by address name)
 
 $result = $data['counter_1'] + ($data['counter_2'] << 16);
@@ -40,4 +47,5 @@ $target = 21464143;
 
 
 var_dump([$result, $target === $result ? "Match" : "Fail"]);
+
 print_r($responseContainer->getErrors());
